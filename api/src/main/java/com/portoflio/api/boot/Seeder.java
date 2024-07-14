1ff602b1872d887d4c4a052dc1207195558318ee
Carolina <crodriguez@iesrafaelalberti.es>
2024-07-14 15:41:04 +0200

    redeploy

--- api/src/main/java/com/portoflio/api/boot/Seeder.java
+++ api/src/main/java/com/portoflio/api/boot/Seeder.java
@@ -20,6 +20,8 @@ public class Seeder implements CommandLineRunner {
         Users user = new Users("user@prueba","Nombre","Apellidos","Dirección 1","Dirección 2","Ciudad","11130","643786543", pass, false);
         userRepository.save(user);
         */
+
+        // ADMIN
         String pass = bCryptPasswordEncoder.encode("password");
         Users admin = new Users("admin", pass, true);
         userRepository.save(admin);
